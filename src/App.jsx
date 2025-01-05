fdda5b9dac3f2decf94031b2b59aaffa29ec105b
Revanshu Pusadkar <revanshu444@gmail.com>
2025-01-05 11:33:39 +0530

    version 2.0 started

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='justify-self-center self-center'>Version 2.0 Starts</h1>
+    </>
+  )
+}
+
+export default App
