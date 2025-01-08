77e44fb526bd6e03eae15af4ca33b0c00d544198
Aaditya Hajari <adityahazari14@gmail.com>
2025-01-08 15:08:46 +0530

    About Section Completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import Navbar from './components/Navbar'
 import Herosection from './section/hero_section/Herosection'
+import Aboutsection from './section/about_section/Aboutsection'
 
 function App() {
 
@@ -8,6 +9,7 @@ function App() {
     <>
       <Navbar/>
       <Herosection/>
+      <Aboutsection/>
     </>
   )
 }
---
+++ src/section/about_section/Aboutsection.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import Aboutnav from './components/Aboutnav';
+import Aboutdescription from './components/Aboutdescription'
+import Aboutfooter from './components/Aboutfooter';
+import './Aboutsection.css';
+
+const Aboutsection = () => {
+  return (
+    <section class="about">
+            <div class="about-div">
+                <div className="scan-line" />
+                <Aboutnav />
+                <Aboutdescription />
+                <Aboutfooter />
+            </div>
+        </section>
+  )
+}
+
+export default Aboutsection
