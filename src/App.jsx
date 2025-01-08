04bcc587632577222b54e8f3b8d4c6d61ab76cad
Revanshu Pusadkar <revanshu444@gmail.com>
2025-01-09 01:23:54 +0530

    social section completed (Animation remains)

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Portfoliosection from './section/portfolio_section/Portfoliosection'
 import Herosection from './section/hero_section/Herosection'
 import Aboutsection from './section/about_section/Aboutsection'
 import Faqsection from './section/faq_section/Faqsection'
+import Socialsection from './section/social_section/Socialsection'
 import './globals.css'
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
       <Herosection/>
       <Aboutsection/>
       <Portfoliosection/>
+      <Socialsection/>
       <Faqsection/>
     </>
   )
