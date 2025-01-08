1a13d09911cec7c2280103437e2f54e87031fa06
Revanshu Pusadkar <revanshu444@gmail.com>
2025-01-08 15:06:44 +0530

    custom cursor added

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import Navbar from './components/Navbar'
 import Portfoliosection from './section/portfolio_section/Portfoliosection'
 import Herosection from './section/hero_section/Herosection'
+import './globals.css'
 
 function App() {
 
