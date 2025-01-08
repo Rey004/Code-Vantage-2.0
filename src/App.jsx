87999c99338266b99211518fff7379bfd2396afd
Revanshu Pusadkar <revanshu444@gmail.com>
2025-01-08 14:03:01 +0530

    minor fixes

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import Navbar from './components/Navbar'
-import Portfoliosection from './sections/portfolio_section/Portfoliosection'
+import Portfoliosection from './section/portfolio_section/Portfoliosection'
 import Herosection from './section/hero_section/Herosection'
 
 function App() {
