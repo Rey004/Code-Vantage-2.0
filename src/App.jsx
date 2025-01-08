819fbb201f6d8d76346dd4fae6143c4f38be66a0
Aaditya Hajari <adityahazari14@gmail.com>
2025-01-09 00:39:28 +0530

    Filler Text Completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
-import { useState } from 'react'
+import React from 'react'
 import Navbar from './components/Navbar'
 import Portfoliosection from './section/portfolio_section/Portfoliosection'
 import Herosection from './section/hero_section/Herosection'
 import Aboutsection from './section/about_section/Aboutsection'
+import Fillertextsection from './section/Filler_text/Fillertextsection'
 import './globals.css'
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
       <Herosection/>
       <Aboutsection/>
       <Portfoliosection/>
+      <Fillertextsection/>
     </>
   )
 }
---
+++ src/section/Filler_text/components/Fillertext.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Fillertext = () => {
+  return (
+    <div className="filler-text">
+        <img className='filler-bracketblue' src="\assets\Bracket Blue.webp" alt="" />
+        <img className='filler-starabove' src="\assets\Star.webp" alt="" />
+            <div style={{padding: "32px 48px", zIndex: "2", position: "relative"}}>
+            Elevating the web<br />
+            experience with<br />
+            next-gen design
+            </div>
+        <img className='filler-starbelow' src="\assets\Star.webp" alt="" />
+        <img className='filler-bracketpurple' src="\assets\Bracket Purple.webp" alt="" />
+    </div>
+  )
+}
+
+export default Fillertext
