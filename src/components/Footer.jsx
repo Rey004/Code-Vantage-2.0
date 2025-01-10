a046f768936ad0d17b6e5218ed48908e2d9e8614
Revanshu Pusadkar <revanshu444@gmail.com>
2025-01-10 23:56:13 +0530

    footer completed

---
+++ src/components/Footer.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import './Footer.css'
+import Button from './Button'
+
+const Footer = () => {
+  return (
+    <footer>
+        <div className="footer-div">
+            <div className="footer-branding">
+                <img src="/assets/Text Logo.webp" alt="" className="secondary-logo" />
+                <img src="/assets/Star.webp" alt="" className="star1" />
+                <img src="/assets/Star.webp" alt="" className="star2" />
+                <img src="/assets/Bracket Purple.webp" alt="" className="bracket-purple" />
+                <img src="/assets/Bracket Blue.webp" alt="" className="bracket-blue" />
+            </div>
+            <div className="footer-content">
+                <div className="info">
+                    <div className="cta">
+                        <p className="cta-para">Code Vantage stands for its trust and quality of work, don’t forget to schedule a call before you leave.</p>
+                        <Button className='footer-button' name="SCHEDULE CALL" />
+                    </div>
+                    <img src="/assets/Icon Logo.webp" alt="" className="footer-icon-logo" />
+                    <div className="emails">
+                        <h2 className='email-title'>Email</h2>
+                        <a href="" className="email">contact@codevantage.in</a>
+                        <a href="" className="email">revanshu@codevantage.in</a>
+                        <a href="" className="email">aaditya@codevantage.in</a>
+                    </div>
+                </div>
+                <p className='rights'>© 2024 codevantage.in All rights reserved.</p>
+                <img src="/assets/Star.webp" alt="" className="stardecor" />
+            </div>
+            <div className="footer-gradient"></div>
+        </div>
+    </footer>
+  )
+}
+
+export default Footer
